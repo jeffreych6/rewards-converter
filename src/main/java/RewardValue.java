f29ecce09554fe5993e3b48b28b43177d0311423
Jeffrey Cheng <jeffreych6@gmail.com>
2023-12-11 15:30:58 -0500

    RewardValueTests added, all test cases passed

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,20 +1,20 @@
 public class RewardValue {
-    private double cashValue;
-    private static final double rate = 0.0035;
+    private final double cashValue;
+    public static final double RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
     public RewardValue(int miles) {
-        this.cashValue = miles * rate;
+        this.cashValue = miles * RATE;
     }
 
     public double getCashValue() {
         return cashValue;
     }
 
-    public double getMilesValue() {
-        return cashValue / rate;
+    public int getMilesValue() {
+        return (int) (cashValue / RATE);
     }
 }
